6310493b9e30fc15fa7fce7c350c294eea6994c2
Ishaan Jain <ishaan160799@gmail.com>
2019-10-20 02:11:30 +0530

    Splash Activity and Logo Applied!

---
+++ app/src/main/java/com/systemtron/finalapp/activities/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.systemtron.finalapp.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.systemtron.finalapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 5000
+
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        mDelayHandler = Handler()
+
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+    override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
+    }
+}
